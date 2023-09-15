fe68a54148cdeed6f1900ed67804ae0f412f1956
Enital <117684114+Enital@users.noreply.github.com>
2023-09-15 12:40:01 +0300

    change Main page

---
+++ src/components/DiaryOnMain/DiaryOnMain.jsx
@@ -0,0 +1,11 @@
+import css from './diaryOnMain.module.css';
+
+const DiaryOnMain = () => {
+  return (
+    <div className={css.diaryMain}>
+      <h2>Diary on Main Page</h2>
+    </div>
+  );
+};
+
+export default DiaryOnMain;
