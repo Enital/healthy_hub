49d6cbc9d2e1d4b4515df11a6f34db8671dca53b
Daryna Karmazin <daryna.karmazin@gmail.com>
2023-09-15 10:53:13 +0300

    Added basic markup to singup page

--- src/pages/SignUpPage/SignUpPage.jsx
+++ src/pages/SignUpPage/SignUpPage.jsx
@@ -1,21 +1,44 @@
-import SignUpForm from 'components/SignUpForm/SignUpForm';
 import SportAndFitnessTrackerIMG from './../../images/img/illustration-sport-and-fitness-tracker.svg';
 import { NavLink } from 'react-router-dom';
+
+import css from './SignUpPage.module.css';
 
 const SignUpPage = () => {
   return (
     <div className="container">
-      <img
-        src={SportAndFitnessTrackerIMG}
-        alt="illustration-sport-and-fitness-tracker"
-      />
-      <div>
-        <h1>Sign up</h1>
-        <h2>You need to register to use the service</h2>
+      <div className={css.wrapper}>
+        <img
+          className={css.img}
+          src={SportAndFitnessTrackerIMG}
+          alt="illustration-sport-and-fitness-tracker"
+        />
+        <div className={css.content}>
+          <h1 className={css.title}>Sign up</h1>
+          <h2 className={css.subtitle}>
+            You need to register to use the service
+          </h2>
+          <form>
+            <label>
+              <input type="text" name="name" placeholder="Name" />
+            </label>
+            <label>
+              <input type="email" name="email" placeholder="E-mail" />
+            </label>
+            <label>
+              <input type="password" name="password" placeholder="Password" />
+            </label>
+            <NavLink className={css.signupBtn} to="">
+              Sign up
+            </NavLink>
+          </form>
+          <div>
+            <p> Do you already have an account?</p>
+            <NavLink className={css.signinBtn} to="/signin">
+              Sign in
+            </NavLink>
+          </div>
+        </div>
       </div>
-      <SignUpForm></SignUpForm>
-      <p> Do you already have an account?</p>
-      <NavLink to="/signin">Sign in</NavLink>
     </div>
   );
 };
