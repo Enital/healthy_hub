4ea1373a612f311414fc4ccdfb8acc92668e8d37
Daryna Karmazin <daryna.karmazin@gmail.com>
2023-09-16 21:17:24 +0300

    Updated private routes

--- src/components/PrivateRoute/PrivateRoute.jsx
+++ src/components/PrivateRoute/PrivateRoute.jsx
@@ -1,11 +1,28 @@
+// import { Navigate } from 'react-router-dom';
+
+// const PrivateRoute = ({ redirectTo = '/', component }) => {
+//   // треба перевірити чи авторизований користувач(чи є токен у глобальному стейті)
+
+//   // змінити умову для авторизованого чи неавторизованого користувача(!auth)
+//   const shouldRedirect = true; //замінити на значення зі стейту
+//   return shouldRedirect ? <Navigate to={redirectTo} /> : component;
+// };
+
+// export default PrivateRoute;
+
 import { Navigate } from 'react-router-dom';
+import { useAuth } from '../../redux/auth/useAuth';
 
-const PrivateRoute = ({ redirectTo = '/', component }) => {
-  // треба перевірити чи авторизований користувач(чи є токен у глобальному стейті)
+/**
+ * - If the route is private and the user is logged in, render the component
+ * - Otherwise render <Navigate> to redirectTo
+ */
 
-  // змінити умову для авторизованого чи неавторизованого користувача(!auth)
-  const shouldRedirect = true; //замінити на значення зі стейту
-  return shouldRedirect ? <Navigate to={redirectTo} /> : component;
+const PrivateRoute = ({ component: Component, redirectTo = '/' }) => {
+  const { isLoggedIn, isRefreshing } = useAuth();
+  const shouldRedirect = !isLoggedIn && !isRefreshing;
+
+  return shouldRedirect ? <Navigate to={redirectTo} /> : Component;
 };
 
 export default PrivateRoute;
