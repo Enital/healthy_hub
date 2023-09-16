401159d78e5eb98019db2bd1a789b436b59b7152
Enital <117684114+Enital@users.noreply.github.com>
2023-09-16 13:02:02 +0300

    Revert "today main page"

--- src/components/Today/Today.jsx
+++ src/components/Today/Today.jsx
@@ -1,19 +1,7 @@
-import { Link } from 'react-router-dom/dist';
-import Food from 'components/Food/Food';
-import Water from 'components/Water/Water';
-import DailyGoal from 'components/DailyGoal/DailyGoal';
 const Today = () => {
   return (
     <div className="today">
-      <div>
-        <h2>Today</h2>
-        <Link to="">On the way to the goal</Link>
-      </div>
-      <div>
-        <DailyGoal />
-        <Water />
-        <Food />
-      </div>
+      <h2>Today on Main Page</h2>
     </div>
   );
 };
