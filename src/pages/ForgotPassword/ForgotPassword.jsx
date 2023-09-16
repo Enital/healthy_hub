6140bf3b7f048e4d8c590c37d3b316436c6763f6
PovaliiLudmila <118770513+PovaliiLudmila@users.noreply.github.com>
2023-09-16 16:26:21 +0300

    1

--- src/pages/ForgotPassword/ForgotPassword.jsx
+++ src/pages/ForgotPassword/ForgotPassword.jsx
@@ -1,9 +1,41 @@
-const ForgotPassword = () => {
-  return (
-    <>
-      <h2>Forgot Password Page</h2>
-    </>
-  );
-};
-
-export default ForgotPassword;
+import { NavLink } from 'react-router-dom';
+import SportAndFitnessTrackerIMG from './../../images/img/illustration-sport-and-fitness-tracker.svg';
+import css from './ForgotPasswort.module.css';
+function ForgotPasswort() {
+    return (
+      <div className={css.wrapper}>
+        <img
+          className={css.img}
+          src={SportAndFitnessTrackerIMG}
+          alt="illustration-sport-and-fitness-tracker"
+        />
+        <div className={css.content}>
+          <h1 className={css.title}>Forgot your password</h1>
+          <h2 className={css.subtitle}>
+          We will send you an email with recovery instructions
+          </h2>
+          <form className={css.form}>
+            <label>
+              <input
+                className={css.input}
+                type="email"
+                name="email"
+                placeholder="E-mail"
+              />
+            </label>
+            <NavLink className={css.send} to="">
+              Send
+            </NavLink>
+          </form>
+          <div className={css.navigation}>
+            <p className={css.question}> Do you already have an account?</p>
+            <NavLink className={css.signinBtn} to="/signin">
+              Sign in
+            </NavLink>
+          </div>
+        </div>
+      </div>
+    );
+  }
+  
+  export default ForgotPasswort;
