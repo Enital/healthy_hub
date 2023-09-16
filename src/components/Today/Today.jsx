baba93be0e697903443263ca08f68d4c58d60f95
EVGENY4510 <lentiay4510z@gmail.com>
2023-09-16 12:56:12 +0300

    today main page

--- src/components/Today/Today.jsx
+++ src/components/Today/Today.jsx
@@ -1,7 +1,19 @@
+import { Link } from 'react-router-dom/dist';
+import Food from 'components/Food/Food';
+import Water from 'components/Water/Water';
+import DailyGoal from 'components/DailyGoal/DailyGoal';
 const Today = () => {
   return (
     <div className="today">
-      <h2>Today on Main Page</h2>
+      <div>
+        <h2>Today</h2>
+        <Link to="">On the way to the goal</Link>
+      </div>
+      <div>
+        <DailyGoal />
+        <Water />
+        <Food />
+      </div>
     </div>
   );
 };
