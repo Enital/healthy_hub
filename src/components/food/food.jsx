b4d1cd720683d4fca9789a0035e820fb95069ad5
Enital <117684114+Enital@users.noreply.github.com>
2023-09-17 12:40:11 +0300

    Revert "main-page"

--- src/components/food/food.jsx
+++ src/components/food/food.jsx
@@ -1,16 +1,8 @@
-import React from 'react';
-
-import { DoughnutChart } from 'functions/DoughnutChart';
-
-export default function Food() {
-  const d = 70;
-  const w = 100;
+export default function food() {
   return (
     <div>
       <h3>Food</h3>
-      <div style={{ width: '300px', height: '300px' }}>
-        {DoughnutChart(d, w)}
-      </div>
+      <div>graph</div>
     </div>
   );
 }
