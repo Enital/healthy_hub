da37c98885339b6d6995797110675dc2fcbb6ce4
EVGENY4510 <lentiay4510z@gmail.com>
2023-09-17 20:24:17 +0300

    main-page 2

--- src/components/Today/Today.jsx
+++ src/components/Today/Today.jsx
@@ -2,6 +2,7 @@ import { Link } from 'react-router-dom';
 import DailyGoal from 'components/dailyGoal/dailyGoal';
 import Water from 'components/water/water';
 import Food from 'components/food/food';
+import arrow from '../../images/icons/arrow-right.svg';
 
 const Today = () => {
   return (
@@ -10,6 +11,7 @@ const Today = () => {
         <h2>Today</h2>
 
         <Link to="">On the way to the goal</Link>
+        <img src={arrow} alt="arrow" />
       </div>
       <div>
         <DailyGoal />
--- src/components/dailyGoal/dailyGoal.jsx
+++ src/components/dailyGoal/dailyGoal.jsx
@@ -1,26 +1,56 @@
 import React from 'react';
+import { useEffect, useState } from 'react';
 import bubble from './../../images/icons/bubble.svg';
-
 import milk from './../../images/icons/milk.svg';
+import axios from 'axios';
+
+const getGoal = async () => {
+  const options = {
+    method: 'GET',
+    url: 'https://goit-healthy-hub.onrender.com/api/user/statistics',
+    headers: {
+      Authorization:
+        'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1MDMxOTExZjE3ZmJhMDY4NzNiMjk1ZiIsImlhdCI6MTY5NDgwMTg5MywiZXhwIjoxNjk3MzkzODkzfQ.OJg9pFwAOeVcYExefy7zABOlOceUW8nwLBaTGnlUokI',
+    },
+  };
+  const { data } = await axios.request(options);
+  return data;
+};
 
 export default function DailyGoal() {
+  const [water, setWater] = useState({});
+  const [calories, setCalories] = useState({});
+  useEffect(() => {
+    (async function getDailyGoal() {
+      try {
+        const loadedGoal = await getGoal();
+        console.log(loadedGoal.total);
+        setWater(loadedGoal.total.water);
+        setCalories(loadedGoal.total.calories);
+      } catch (error) {}
+    })();
+  }, []);
+
+  const waterGoal = water.goal;
+  const caloriesGoal = calories.goal;
+
   return (
     <div>
       <h3>Daily goal</h3>
-      <div>graph</div>
+
       <div>
         <div>
-          <img src={bubble} alt="illustration-summer-hiking" />
+          <img src={bubble} alt="illustration" />
           <div>
             <h3>Calories</h3>
-            <p>1700</p>
+            <p>{caloriesGoal}</p>
           </div>
         </div>
         <div>
-          <img src={milk} alt="illustration-summer-hiking" />
+          <img src={milk} alt="illustration" />
           <div>
             <h3>Water</h3>
-            <p>1500 ml</p>
+            <p>{waterGoal} ml</p>
           </div>
         </div>
       </div>
