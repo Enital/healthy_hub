42723f3dd6a525c801319f8d970633831afc0b89
EVGENY4510 <lentiay4510z@gmail.com>
2023-09-17 15:44:55 +0300

    new main-page

--- src/components/food/food.jsx
+++ src/components/food/food.jsx
@@ -1,8 +1,17 @@
-export default function food() {
+import React from 'react';
+
+import { DoughnutChart } from '../Charts/DoughnutChart/DoughnutChart';
+
+export default function Food() {
+  const d = 70;
+  const w = 100;
   return (
     <div>
       <h3>Food</h3>
       <div>graph</div>
+      <div style={{ width: '168px', height: '168px' }}>
+        {DoughnutChart(d, w)}
+      </div>
     </div>
   );
 }
