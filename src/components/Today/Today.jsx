a6e2acb3464575ea8c63becd9be37b6fbb1c394c
EVGENY4510 <lentiay4510z@gmail.com>
2023-09-18 13:22:18 +0300

    main-page-2

--- src/components/Today/Today.jsx
+++ src/components/Today/Today.jsx
@@ -1,17 +1,29 @@
+import React from 'react';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
 import DailyGoal from 'components/dailyGoal/dailyGoal';
 import Water from 'components/water/water';
 import Food from 'components/food/food';
 import arrow from '../../images/icons/arrow-right.svg';
+import { fetchGoals } from 'redux/usersGoal/operations';
 
 const Today = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchGoals());
+  }, [dispatch]);
+
   return (
     <div className="today">
       <div>
         <h2>Today</h2>
 
-        <Link to="">On the way to the goal</Link>
-        <img src={arrow} alt="arrow" />
+        <div>
+          <Link to="">On the way to the goal</Link>
+          <img src={arrow} alt="arrow" />
+        </div>
       </div>
       <div>
         <DailyGoal />
--- src/components/dailyGoal/dailyGoal.jsx
+++ src/components/dailyGoal/dailyGoal.jsx
@@ -1,39 +1,17 @@
 import React from 'react';
-import { useEffect, useState } from 'react';
 import bubble from './../../images/icons/bubble.svg';
 import milk from './../../images/icons/milk.svg';
-import axios from 'axios';
-
-const getGoal = async () => {
-  const options = {
-    method: 'GET',
-    url: 'https://goit-healthy-hub.onrender.com/api/user/statistics',
-    headers: {
-      Authorization:
-        'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1MDMxOTExZjE3ZmJhMDY4NzNiMjk1ZiIsImlhdCI6MTY5NDgwMTg5MywiZXhwIjoxNjk3MzkzODkzfQ.OJg9pFwAOeVcYExefy7zABOlOceUW8nwLBaTGnlUokI',
-    },
-  };
-  const { data } = await axios.request(options);
-  return data;
-};
+import { selectGoals } from 'redux/usersGoal/selectors';
+import { useSelector } from 'react-redux';
 
 export default function DailyGoal() {
-  const [water, setWater] = useState({});
-  const [calories, setCalories] = useState({});
-  useEffect(() => {
-    (async function getDailyGoal() {
-      try {
-        const loadedGoal = await getGoal();
-        console.log(loadedGoal.total);
-        setWater(loadedGoal.total.water);
-        setCalories(loadedGoal.total.calories);
-      } catch (error) {}
-    })();
-  }, []);
+  const { items } = useSelector(selectGoals);
 
-  const waterGoal = water.goal;
-  const caloriesGoal = calories.goal;
-
+  if (Object.keys(items).length === 0) {
+    return;
+  }
+  const waterGoal = items.total.water.goal;
+  const caloriesGoal = items.total.calories.goal;
   return (
     <div>
       <h3>Daily goal</h3>
