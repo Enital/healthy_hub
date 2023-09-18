6361a7a6409fbc4e2e3f2871e3da5d2071d17bc1
PovaliiLudmila <118770513+PovaliiLudmila@users.noreply.github.com>
2023-09-18 20:51:12 +0300

    1

--- src/pages/ForgotPassword/ForgotPassword.jsx
+++ src/pages/ForgotPassword/ForgotPassword.jsx
@@ -1,8 +1,38 @@
 import { NavLink } from 'react-router-dom';
 import SportAndFitnessTrackerIMG from './../../images/img/illustration-sport-and-fitness-tracker.svg';
 import css from './ForgotPassword.module.css';
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { logIn } from 'redux/auth/operations';
+
 
 function ForgotPassword() {
+  const [email, setEmail] = useState('');
+  const handleChange = event => {
+    const { name, value } = event.target;
+
+    switch (name) {
+      case 'email':
+        setEmail(value);
+        break;
+      default:
+        return;
+    }
+  };
+  const dispatch = useDispatch();
+  const handleSubmit = e => {
+    e.preventDefault();
+    const form = e.currentTarget;
+    dispatch(
+      logIn({
+        email,
+      })
+    );
+    form.reset();
+  };
+      fetch("http://goit-healthy-hub.onrender.com/api/auth/restore/"), {
+      method: "POST",
+    }}
     return (
       <div className={css.container}>
       <div className={css.wrapper}>
@@ -16,16 +46,18 @@ function ForgotPassword() {
           <h2 className={css.subtitle}>
           We will send you an email with recovery instructions
           </h2>
-          <form className={css.form}>
+          <form className={css.form} onSubmit={handleSubmit}>
             <label>
               <input
                 className={css.input}
                 type="email"
                 name="email"
                 placeholder="E-mail"
+                value={email}
+                onChange={handleChange}  
               />
             </label>
-            <NavLink className={css.send} to="/">
+            <NavLink className={css.send} >
               Send
             </NavLink>
           </form>
