4e69c7a90101996762246e266a82ea09c00414bb
Daryna Karmazin <daryna.karmazin@gmail.com>
2023-09-19 16:21:27 +0300

    Added RecFoodOnMain component

--- src/components/RecFoodOnMain/RecFoodOnMain.jsx
+++ src/components/RecFoodOnMain/RecFoodOnMain.jsx
@@ -1,9 +1,32 @@
-import css from './recFoodOnMain.module.css';
+import RecommendedFoodList from 'components/RecommendedFoodList/RecommendedFoodList';
+import css from './RecFoodOnMain.module.css';
+import { useEffect, useState } from 'react';
+import getRecomendations from '../../redux/products/operations';
+
+import { useSelector } from 'react-redux';
+import { NavLink } from 'react-router-dom';
 
 const RecFoodOnMain = () => {
+  const [recomendations, setRecomendations] = useState([]);
+  const token = useSelector(state => state.auth.token);
+
+  useEffect(() => {
+    getRecomendations(token, 4)
+      .then(data => {
+        setRecomendations(data);
+      })
+      .catch(err => console.error('error:' + err));
+  }, [token]);
+
   return (
-    <div className={css.recFoodMain}>
-      <h2>Recommended Food on Main Pge</h2>
+    <div className={css.container}>
+      <h1 className={css.title}>Recommended food</h1>
+      <div className={css.wrapper}>
+        <RecommendedFoodList products={recomendations} />
+      </div>
+      <NavLink className={css.link} to="/recommended-food">
+        Sign in &#8594;
+      </NavLink>
     </div>
   );
 };
