5c03dd98bceb49cf0b46ab7a34c3b6a4c03d31eb
Daryna Karmazin <daryna.karmazin@gmail.com>
2023-09-19 16:22:31 +0300

    Added RecFoodOnMain component

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,13 +1,16 @@
 import Today from 'components/Today/Today';
 import DiaryOnMain from 'components/DiaryOnMain/DiaryOnMain';
 import RecFoodOnMain from 'components/RecFoodOnMain/RecFoodOnMain';
+import css from './Main.module.css';
 
 const Main = () => {
   return (
     <div>
-      <Today />
-      <DiaryOnMain />
-      <RecFoodOnMain />
+       <Today />
+      <div className={css.lowContainer}>
+        <DiaryOnMain />
+        <RecFoodOnMain />
+      </div>
     </div>
   );
 };
