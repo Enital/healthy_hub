b645fc055a34a43ee5d44f881b437a1d7a79696c
Vozniak-Ihor <118538389+Vozniak-Ihor@users.noreply.github.com>
2023-09-19 21:22:34 +0300

    Update save form

--- src/components/SelectGenderAge/SelectGenderAge.jsx
+++ src/components/SelectGenderAge/SelectGenderAge.jsx
@@ -1,16 +1,32 @@
 import { useState } from 'react';
 import genderAndAgeIMG from './../../images/img/illustration-gender-and-age.svg';
 import css from './SelectGenderAge.module.css';
+// import { Formik, Form, Field, ErrorMessage } from 'formik';
+// import * as yup from 'yup';
+// import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 function SelectGenderAge({ onForm, onBackPage, age, gender }) {
   const [genderValue, setGender] = useState(gender);
   const [ageValue, setAge] = useState(age);
+
   const handleChangeAge = e => {
     setAge(e.target.value);
   };
   const handleChangeGender = e => {
     setGender(e.target.value);
   };
+
+  // const schema = yup.object().shape({
+  //   age: yup.number().required('Age is required'),
+  // });
+  // const initialValues = {
+  //   age: '',
+  // };
+  // const FromError = ({ name }) => {
+  //   return (
+  //     <ErrorMessage name={name} render={message => Notify.failure(message)} />
+  //   );
+  // };
   return (
     <div className={css.wrapper}>
       <img
@@ -23,48 +39,55 @@ function SelectGenderAge({ onForm, onBackPage, age, gender }) {
         <h2 className={css.subtitle}>
           Choose a goal so that we can help you effectively
         </h2>
-        <form className={css.form} onSubmit={onForm}>
-          <p>Gender</p>
-          <div className={css.wrappers}>
-            <label className={css.label}>
+        {/* <Formik
+          initialValues={initialValues}
+          onSubmit={onForm}
+          validationSchema={schema}
+        > */}
+          <form className={css.form} onSubmit={onForm}>
+            <p>Gender</p>
+            <div className={css.wrappers}>
+              <label className={css.label}>
+                <input
+                  type="radio"
+                  name="gender"
+                  value="male"
+                  checked={genderValue === 'male'}
+                  onChange={handleChangeGender}
+                />
+                Male
+              </label>
+              <label className={css.label}>
+                <input
+                  type="radio"
+                  name="gender"
+                  value="female"
+                  checked={genderValue === 'female'}
+                  onChange={handleChangeGender}
+                />
+                Female
+              </label>
+            </div>
+            <label className={css.labelAge}>
+              Your age
               <input
-                type="radio"
-                name="gender"
-                value="male"
-                checked={genderValue === 'male'}
-                onChange={handleChangeGender}
+                className={css.input}
+                type="text"
+                name="age"
+                placeholder="Enter your age"
+                value={ageValue}
+                onChange={handleChangeAge}
               />
-              Male
+              {/* <FromError name="age" /> */}
             </label>
-            <label className={css.label}>
-              <input
-                type="radio"
-                name="gender"
-                value="female"
-                checked={genderValue === 'female'}
-                onChange={handleChangeGender}
-              />
-              Female
-            </label>
-          </div>
-          <label className={css.labelAge}>
-            Your age
-            <input
-              className={css.input}
-              type="text"
-              name="age"
-              placeholder="Enter your age"
-              value={ageValue}
-              onChange={handleChangeAge}
-            />
-          </label>
-          <button className={css.NextBtn} type="submit">
-            Next
-          </button>
-          <button className={css.BackBtn} onClick={onBackPage} type="button">
-            Back
-          </button>
-        </form>
+            <button className={css.NextBtn} type="submit">
+              Next
+            </button>
+            <button className={css.BackBtn} onClick={onBackPage} type="button">
+              Back
+            </button>
+          </form>
+        {/* </Formik> */}
       </div>
     </div>
   );
--- src/components/SignUpForm/SignUpForm.jsx
+++ src/components/SignUpForm/SignUpForm.jsx
@@ -1,13 +1,15 @@
 import { NavLink } from 'react-router-dom';
 import SportAndFitnessTrackerIMG from './../../images/img/illustration-sport-and-fitness-tracker.svg';
 import css from './SignUpForm.module.css';
-
 import { useState } from 'react';
+// import { Formik, Form, Field, ErrorMessage } from 'formik';
+// import * as yup from 'yup';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
-function SignUpForm({ onForm }) {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+function SignUpForm({ onForm, name2, email2, password2 }) {
+  const [name, setName] = useState(name2);
+  const [email, setEmail] = useState(email2);
+  const [password, setPassword] = useState(password2);
 
   const handleChange = event => {
     const { name, value } = event.target;
@@ -26,7 +28,40 @@ function SignUpForm({ onForm }) {
         return;
     }
   };
+  function myFunction() {
+    const x = document.getElementById('myInput');
+    if (x.type === 'password') {
+      x.type = 'text';
+    } else {
+      x.type = 'password';
+    }
+  }
+  // const schema = yup.object().shape({
+  //   name: yup.string().required('Name is required'),
+  //   email: yup
+  //     .string()
+  //     .email('Invalid email format')
+  //     .required('Email is required'),
+  //   password: yup
+  //     .string()
+  //     .matches(
+  //       /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,16}$/,
+  //       'Password must meet the criteria (min. 6, max. 16 characters, at least 1 digit, 1 lowercase letter, and 1 uppercase letter)'
+  //     )
+  //     .required('Password is required'),
+  // });
 
+  // const initialValues = {
+  //   name: '',
+  //   email: '',
+  //   password: '',
+  // };
+
+  // const FromError = ({ name }) => {
+  //   return (
+  //     <ErrorMessage name={name} render={message => Notify.failure(message)} />
+  //   );
+  // }
   return (
     <div className={css.wrapper}>
       <img
@@ -39,6 +74,11 @@ function SignUpForm({ onForm }) {
         <h2 className={css.subtitle}>
           You need to register to use the service
         </h2>
+        {/* <Formik
+          initialValues={initialValues}
+          onSubmit={onForm}
+          validationSchema={schema}
+        > */}
         <form className={css.form} onSubmit={onForm}>
           <label>
             <input
@@ -48,8 +88,8 @@ function SignUpForm({ onForm }) {
               placeholder="Name"
               value={name}
               onChange={handleChange}
-              required
             />
+            {/* <FromError name="name" /> */}
           </label>
           <label>
             <input
@@ -61,24 +101,34 @@ function SignUpForm({ onForm }) {
               onChange={handleChange}
               required
             />
+            {/* <FromError name="email" /> */}
           </label>
-          <label>
+          <label className={css.label}>
             <input
               className={css.input}
               type="password"
               name="password"
               placeholder="Password"
               value={password}
+              id="myInput"
               onChange={handleChange}
+              pattern="^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,16}$"
+              title="Must contain at least one number and one uppercase and lowercase letter, and at least 6 to 16 characters"
               required
-              pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,16}"
-              title="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters"
+            />
+            {/* <FromError name="password" /> */}
+            <input
+              className={css.checkbox}
+              type="checkbox"
+              onChange={myFunction}
+              style={{ display: password ? 'block' : 'none' }}
             />
           </label>
           <button className={css.signupBtn} type="submit">
             Next
           </button>
         </form>
+        {/* </Formik> */}
         <div className={css.questionTrumb}>
           <p className={css.question}> Do you already have an account?</p>
           <NavLink className={css.signinBtn} to="/signin">
