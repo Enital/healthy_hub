4efaec42d11d9ba127ab69ce531072b3c174b621
Enital <117684114+Enital@users.noreply.github.com>
2023-09-19 22:43:19 +0300

    weight chart rebuild

--- src/components/Charts/WeightChart/BuildWeightChart.jsx
+++ src/components/Charts/WeightChart/BuildWeightChart.jsx
@@ -43,24 +43,15 @@ const data = [
 function BuildWeightChart() {
   return (
     <>
-      {/* {images.map(image => {
-        return (
-          <li className={css.item} key={image.id}>
-            <div onClick={() => setSelectedImage(image)}>
-              <ImageGalleryItem oneImage={image}></ImageGalleryItem>
-            </div>
-          </li>
-      })} */}
-      {/* {data.map((item, index) => { */}
       <div className={css.weightTable}>
         {data.map((item, index) => (
           <div className={css.tableBlock} key={index}>
-            <tr className={css.tableUpRow}>
-              <td>{item}</td>
-            </tr>
-            <tr className={css.tableDownRow}>
-              <td>{index + 1}</td>
-            </tr>
+            <p className={css.tableUpRow}>
+              <span>{item}</span>
+            </p>
+            <p className={css.tableDownRow}>
+              <span>{index + 1}</span>
+            </p>
           </div>
         ))}
       </div>
