ddf6a37372cbb812c10177803b0b73b05ff72486
PovaliiLudmila <118770513+PovaliiLudmila@users.noreply.github.com>
2023-09-19 18:01:59 +0300

    1

--- src/pages/ForgotPassword/ForgotPassword.jsx
+++ src/pages/ForgotPassword/ForgotPassword.jsx
@@ -2,34 +2,36 @@ import { NavLink } from 'react-router-dom';
 import SportAndFitnessTrackerIMG from './../../images/img/illustration-sport-and-fitness-tracker.svg';
 import css from './ForgotPassword.module.css';
 import { useState } from 'react';
-import { useDispatch } from 'react-redux';
-import { logIn } from 'redux/auth/operations';
+// import { useDispatch } from 'react-redux';
+// import { logIn } from 'redux/auth/operations';
 
 
 function ForgotPassword() {
   const [email, setEmail] = useState('');
-  const handleChange = event => {
-    const { name, value } = event.target;
-
-    switch (name) {
-      case 'email':
-        setEmail(value);
-        break;
-      default:
-        return;
-    }
-  };
-  const dispatch = useDispatch();
-  const handleSubmit = e => {
+  const [message, setMessage] = useState('');
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    const form = e.currentTarget;
-    dispatch(
-      logIn({
-        email,
-      })
-    );
-    form.reset();
-  };
+
+    try {
+      const response = await fetch('http://goit-healthy-hub.onrender.com/api/auth/restore/forgotpassword', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ email }),
+      });
+
+      const data = await response.json();
+
+      if (response.ok) {
+        setMessage(data.message);
+      } else {
+        setMessage(data.error);
+      }
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  }
   //   fetch("http://goit-healthy-hub.onrender.com/api/auth/restore/"), {
   //   method: "POST",
   // };
@@ -54,13 +56,14 @@ function ForgotPassword() {
                 name="email"
                 placeholder="E-mail"
                 value={email}
-                onChange={handleChange}  
+                onChange={(e) => setEmail(e.target.value)}  
               />
             </label>
             <NavLink className={css.send} >
               Send
             </NavLink>
           </form>
+          {message && <p>{message}</p>}
           <div className={css.navigation}>
             <p className={css.question}> Do you already have an account?</p>
             <NavLink className={css.signinBtn} to="/signin">
