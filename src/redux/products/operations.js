4294ff9938ce04e28692c280cba07106389e5ad4
Daryna Karmazin <daryna.karmazin@gmail.com>
2023-09-19 14:14:27 +0300

    Updated recomendedFood page

--- src/redux/products/operations.js
+++ src/redux/products/operations.js
@@ -1,7 +1,10 @@
 import axios from 'axios';
 
-const token =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1MDMxOTExZjE3ZmJhMDY4NzNiMjk1ZiIsImlhdCI6MTY5NDgwMTg5MywiZXhwIjoxNjk3MzkzODkzfQ.OJg9pFwAOeVcYExefy7zABOlOceUW8nwLBaTGnlUokI';
+// const token =
+//   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1MDMxOTExZjE3ZmJhMDY4NzNiMjk1ZiIsImlhdCI6MTY5NDgwMTg5MywiZXhwIjoxNjk3MzkzODkzfQ.OJg9pFwAOeVcYExefy7zABOlOceUW8nwLBaTGnlUokI';
+
+const token = localStorage.getItem('persist:auth');
+console.log(token);
 
 axios.defaults.baseURL = 'https://goit-healthy-hub.onrender.com/api';
 axios.defaults.headers.common.Authorization = `Bearer ${token}`;
