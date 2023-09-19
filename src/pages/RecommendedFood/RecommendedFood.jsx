ff6e3b18d08e2e29d62f6f51dff1a5ca3dc0d07f
Daryna Karmazin <daryna.karmazin@gmail.com>
2023-09-19 10:43:29 +0300

    Added markup for Recommended food page

--- src/pages/RecommendedFood/RecommendedFood.jsx
+++ src/pages/RecommendedFood/RecommendedFood.jsx
@@ -1,8 +1,21 @@
+import RecommendedFoodList from 'components/RecommendedFoodList/RecommendedFoodList';
+import ketogenicDietIMG from './../../images/img/illustration-ketogenic-diet.svg';
+import css from './RecommendedFood.module.css';
+import recomendations from './goit-healthy-hub-db.recomendations';
+
 const RecommendedFood = () => {
   return (
-    <>
-      <h2>Recommended Food</h2>
-    </>
+    <div className="container">
+      <h1 className={css.title}>Recommented food</h1>
+      <div className={css.wrapper}>
+        <img
+          className={css.img}
+          src={ketogenicDietIMG}
+          alt="illustration-ketogenic-diet"
+        />
+        <RecommendedFoodList products={recomendations} />
+      </div>
+    </div>
   );
 };
 
