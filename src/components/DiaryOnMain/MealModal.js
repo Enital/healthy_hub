3c3b586d7099dccd9730d215d301e46537341061
MariySemenenko <mashyni92@gmail.com>
2023-09-24 19:57:04 +0300

    //import

--- src/components/DiaryOnMain/MealModal.js
+++ src/components/DiaryOnMain/MealModal.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import breakfast from '../../images/illustration/breakfast-image.svg';
 import { FaBitbucket } from "react-icons/fa";
+//import { FaBitbucket } from "react-icons/fa";
 import css from './diaryOnMain.module.css';
 import { useDispatch } from 'react-redux';
 import { fetchGoalsConfirm } from 'redux/usersGoal/operations';
