d1fcdf8aa2210b1a625e6fcb25a5b68ad0caf601
Enital <117684114+Enital@users.noreply.github.com>
2023-09-27 11:09:34 +0300

    Revert "Revert "update scroll& styles""
    This reverts commit 611a0c016d4e4971a32abe9ac389c7b2c6e1f282.

--- src/components/Charts/WeightChart/WeightChart.jsx
+++ src/components/Charts/WeightChart/WeightChart.jsx
@@ -1,12 +1,14 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { selectCharts } from 'redux/dashboard/selectors';
 import BuildWeightChart from './BuildWeightChart';
 import BuildWeightChartYear from './BuildWeightChartYear';
 import { useSelector } from 'react-redux';
-import SimpleBar from 'simplebar-react';
+// import 'overlayscrollbars/overlayscrollbars.css';
+// import { OverlayScrollbars } from 'overlayscrollbars';
+// import SimpleBar from 'simplebar-react';
 
 import css from './weightChart.module.css';
-import 'simplebar-react/dist/simplebar.min.css';
+// import 'simplebar-react/dist/simplebar.min.css';
 
 const _ = require('lodash');
 
@@ -14,50 +16,69 @@ export default function WeightChart({ showMonth }) {
   const { graph } = useSelector(selectCharts);
 
   const data = graph.weight;
-
+  // const tableContainerRef = useRef(null);
+  // useEffect(() => {
+  //   if (tableContainerRef.current) {
+  //     OverlayScrollbars(tableContainerRef.current, {});
+  //   }
+  // }, []);
   const processedData = data.filter(item => {
     return item > 0;
   });
   // console.log(processedData);
   const average = _.mean(processedData).toFixed(1);
-  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
-  useEffect(() => {
-    window.onresize = () => {
-      setWindowWidth(window.innerWidth);
-      return () => {
-        windowWidth.onresize = false;
-      };
-    };
-  }, [windowWidth]);
-  if (windowWidth > 834) {
-    return (
-      <div className={css.weightChart}>
-        <div className={css.weightTitle}>
-          <p className={css.chartTitle}>Weight</p>
-          <p className={css.chartSubtitle}>
-            Average value: <span className={css.average}>{average} kg</span>
-          </p>
-        </div>
-        <SimpleBar style={{ maxWidth: 310 }}>
-          <div className={css.chart}>
-            {showMonth ? <BuildWeightChart /> : <BuildWeightChartYear />}
-          </div>
-        </SimpleBar>
+
+  // const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  // useEffect(() => {
+  //   window.onresize = () => {
+  //     setWindowWidth(window.innerWidth);
+  //     return () => {
+  //       windowWidth.onresize = false;
+  //     };
+  //   };
+  // }, [windowWidth]);
+  // if (windowWidth > 834) {
+  //   return (
+  //     <div className={css.weightChart}>
+  //       <div className={css.weightTitle}>
+  //         <p className={css.chartTitle}>Weight</p>
+  //         <p className={css.chartSubtitle}>
+  //           Average value: <span className={css.average}>{average} kg</span>
+  //         </p>
+  //       </div>
+  //       <SimpleBar style={{ maxWidth: 310 }}>
+  //         <div className={css.chart}>
+  //           {showMonth ? <BuildWeightChart /> : <BuildWeightChartYear />}
+  //         </div>
+  //       </SimpleBar>
+  //     </div>
+  //   );
+  // } else {
+  //   return (
+  //     <div className={css.weightChart}>
+  //       <div className={css.weightTitle}>
+  //         <p className={css.chartTitle}>Weight</p>
+  //         <p className={css.chartSubtitle}>
+  //           Average value: <span className={css.average}>{average} kg</span>
+  //         </p>
+  //       </div>
+  //       <div className={css.chart}>
+  //         {showMonth ? <BuildWeightChart /> : <BuildWeightChartYear />}
+  //       </div>
+  //     </div>
+  //   );
+  // }
+  return (
+    <div className={css.weightChart}>
+      <div className={css.weightTitle}>
+        <p className={css.chartTitle}>Weight</p>
+        <p className={css.chartSubtitle}>
+          Average value: <span className={css.average}>{average} kg</span>
+        </p>
       </div>
-    );
-  } else {
-    return (
-      <div className={css.weightChart}>
-        <div className={css.weightTitle}>
-          <p className={css.chartTitle}>Weight</p>
-          <p className={css.chartSubtitle}>
-            Average value: <span className={css.average}>{average} kg</span>
-          </p>
-        </div>
-        <div className={css.chart}>
-          {showMonth ? <BuildWeightChart /> : <BuildWeightChartYear />}
-        </div>
+      <div className={css.chart}>
+        {showMonth ? <BuildWeightChart /> : <BuildWeightChartYear />}
       </div>
-    );
-  }
+    </div>
+  );
 }
